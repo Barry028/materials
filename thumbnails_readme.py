fcc40a93491daec9acda3e9e3befa4284250c75a
Barry Lin <39817188+Barry028@users.noreply.github.com>
2026-02-27 12:15:46 +0800

    add

--- thumbnails_readme.py
+++ thumbnails_readme.py
@@ -1,40 +1,36 @@
-"""Example for thumbnails_readme.py."""
-
-# Linux - sudo apt-get install poppler-utils
-from pathlib import Path
+"""Action for thumbnails_readme.py."""
 
 from thumbnails_readme import thumbnails_readme
+from pathlib import Path
 
 # Maximum thumbnail size - reduce to reduce final thumbnail size
 max_size = (128, 128)
 # PDF quality, lower the number, lower the quality
 pdf_quality = 15
 
-# Windows
-poppler_path = r'C:\Program Files\poppler-0.68.0\bin'
 # Linux
 poppler_path = None
 
+# path
 path = Path.cwd()
-path = path.parent
 
 # Do not forget to add your path to image thumbnails folder
 skiplist = (
-    '.git',
-    '.github',
-    '.idea',
-    'idea',
-    'image_thumbnails',
-    '.pytest_cache',
-    'thumbnails_readme',
-    'venv',
-    'docs',
+    ".git",
+    ".github",
+    ".idea",
+    "idea",
+    "image_thumbnails",
+    ".pytest_cache",
+    "thumbnails_readme",
+    "venv",
 )
 
+# WSL path
 path_to_thumbnails_folder = path / 'image_thumbnails'
 path_to_readme = Path(path / 'README.md')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     thumbnails_readme.prepare_thumbnails_folder(path_to_thumbnails_folder)
     thumbnails_readme.prepare_readme(path_to_readme)
     thumbnails_readme.crawl(
